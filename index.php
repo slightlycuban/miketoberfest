57270fc256e46f63603ea0343e39a2b446397908
Michael Tracy <slightlycuban@gmail.com>
2014-09-20 12:39:52 -0400

    Start creating a landing page for Miketoberfest

---
+++ index.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+  <title>Mike in 2014: the beerening</title>
+</head>
+<body>
+  <header>
+    <h1 class="logo">Miketoberfest <small>2014</small></h1>
+    <p>Come join the celebration of Mike's glorious return to the sunshine state!</p>
+    <p class="date">Saturday, October 25, 2014</p>
+    <a href="#contact">Sign-up Now</a>
+  </header>
+  <section id="location">
+    <dl>
+      <dt>When</dt><dd>Saturday, October 25, 2014, 3-7 pm</dd>
+      <dt>Where</dt><dd>Sand Point Park, Titusville, FL</dd>
+  </section>
+  <section id="beers">
+    <h2>On Tap</h2>
+    <div class="otw">
+      <h3>Off the wall</h3>
+      <p>Bright, summery, and strong. The crowd-pleaser is back</p>
+    </div>
+    <div class="hishe">
+      <h3>How It Should Have Ended</h3>
+      <p>One year ago I started this brewing experiment. Now I revisit my first disaster-of-a-beer in true Floridian Oktoberfest style</p>
+    </div>
+    <div class="psa">
+      <h3>Pumkin Spice Ale</h3>
+      <p>It's the time of year for Pumpkin-Spice all-the-things. I felt obligated to comply.</p>
+    </div>
+  </section>
+  <section id="food">
+    <h2>Foods included</h2>
+    <ul>
+      <li>Pretzels</li>
+      <li>Garlic rolls</li>
+      <li>Beer!</li>
+    </ul>
+  </section>
+  <section id="about">
+    <div class="venue">
+    </div>
+    <div class="themike">
+    </div>
+  </section>
+  <footer>
+  </footer>
+</body>
+</html>
