4f3d02bf3855ebdb87c8c69e1106fc413bc49e0f
Michael Tracy <slightlycuban@gmail.com>
2014-09-21 10:46:05 -0400

    Link to Foundatoin css (and reword the PSA)

--- index.php
+++ index.php
@@ -1,50 +1,62 @@
 <!DOCTYPE html>
-<html>
-<head>
-  <title>Mike in 2014: the beerening</title>
-</head>
-<body>
-  <header>
-    <h1 class="logo">Miketoberfest <small>2014</small></h1>
-    <p>Come join the celebration of Mike's glorious return to the sunshine state!</p>
-    <p class="date">Saturday, October 25, 2014</p>
-    <a href="#contact">Sign-up Now</a>
-  </header>
-  <section id="location">
-    <dl>
-      <dt>When</dt><dd>Saturday, October 25, 2014, 3-7 pm</dd>
-      <dt>Where</dt><dd>Sand Point Park, Titusville, FL</dd>
-  </section>
-  <section id="beers">
-    <h2>On Tap</h2>
-    <div class="otw">
-      <h3>Off the wall</h3>
-      <p>Bright, summery, and strong. The crowd-pleaser is back</p>
-    </div>
-    <div class="hishe">
-      <h3>How It Should Have Ended</h3>
-      <p>One year ago I started this brewing experiment. Now I revisit my first disaster-of-a-beer in true Floridian Oktoberfest style</p>
-    </div>
-    <div class="psa">
-      <h3>Pumkin Spice Ale</h3>
-      <p>It's the time of year for Pumpkin-Spice all-the-things. I felt obligated to comply.</p>
-    </div>
-  </section>
-  <section id="food">
-    <h2>Foods included</h2>
-    <ul>
-      <li>Pretzels</li>
-      <li>Garlic rolls</li>
-      <li>Beer!</li>
-    </ul>
-  </section>
-  <section id="about">
-    <div class="venue">
-    </div>
-    <div class="themike">
-    </div>
-  </section>
-  <footer>
-  </footer>
-</body>
+<html class="no-js" lang="en">
+  <head>
+    <meta charset="utf-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Mike in 2014: the beerening</title>
+    <link rel="stylesheet" href="css/normalize.css" />
+    <link rel="stylesheet" href="css/foundation.css" />
+    <script src="js/vendor/modernizr.js"></script>
+  </head>
+  <body>
+    <header>
+      <h1 class="logo">Miketoberfest <small>2014</small></h1>
+      <p>Come join the celebration of Mike's glorious return to the sunshine state!</p>
+      <p class="date">Saturday, October 25, 2014</p>
+      <a href="#contact">Sign-up Now</a>
+    </header>
+    <section id="location">
+      <h2>Where I'm having this thing</h2>
+      <dl>
+        <dt>When</dt><dd>Saturday, October 25, 2014, 3-7 pm</dd>
+        <dt>Where</dt><dd>Sand Point Park, Titusville, FL</dd>
+      </dl>
+    </section>
+    <section id="beers">
+      <h2>On Tap</h2>
+      <div class="otw">
+        <h3>Off the wall</h3>
+        <p>Bright, summery, and strong. The crowd-pleaser is back</p>
+      </div>
+      <div class="hishe">
+        <h3>How It Should Have Ended</h3>
+        <p>One year ago I started this brewing experiment. Now I revisit my first disaster-of-a-beer in true Floridian Oktoberfest style</p>
+      </div>
+      <div class="psa">
+        <h3>The Angst</h3>
+        <p>My Pumpkin Spice Ale. Ugh, its so mainstream.</p>
+      </div>
+    </section>
+    <section id="food">
+      <h2>Foods included</h2>
+      <ul>
+        <li>Pretzels</li>
+        <li>Garlic rolls</li>
+        <li>Beer!</li>
+      </ul>
+    </section>
+    <section id="about">
+      <div class="venue">
+      </div>
+      <div class="themike">
+      </div>
+    </section>
+    <footer>
+    </footer>
+   <script src="js/vendor/jquery.js"></script>
+   <script src="js/foundation.min.js"></script>
+   <script>
+     $(document).foundation();
+   </script>
+  </body>
 </html>
