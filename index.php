8c2f6fe42de18e6e45db9c3d011a1603c01a6a55
Michael Tracy <slightlycuban@gmail.com>
2015-04-08 23:16:22 -0400

    Update the layout for Spring

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Mike in 2014: the beerening</title>
+    <title>Mike's Spring Brew</title>
     <link rel="stylesheet" href="css/normalize.css" />
     <link rel="stylesheet" href="css/foundation.css" />
     <link rel="stylesheet" href="css/app.css" />
@@ -13,7 +13,7 @@
     <script src="js/vendor/modernizr.js"></script>
   </head>
   <body>
-    <div class="contain-to-grid">
+    <div id="nav" class="contain-to-grid">
       <nav class="top-bar" data-topbar role="navigation">
         <section class="top-bar-section">
           <ul>
@@ -26,20 +26,19 @@
     </div>
     <header>
       <div class="row">
-        <h1 class="logo">Mike<wbr/>tober<wbr/>fest <small>2014</small></h1>
-        <p>Come join the celebration of Mike's glorious return to the sunshine state!</p>
-        <p class="date">Saturday, October 25, 2014</p>
-        <p><a class="button" href="http://twitter.com/intent/tweet?text=See you there! @SlightlyCuban&hashtags=miketoberfest">I'm Coming!</a></p>
+        <h1 class="logo">Spring Brew</h1>
+        <p>Spring is here and so is Mike's Beer</p>
+        <p class="date">Saturday, April 11, 2015</p>
+        <p><a class="button" href="http://twitter.com/intent/tweet?text=See you there! @SlightlyCuban&hashtags=mikebeer">I'm Coming!</a></p>
       </div>
     </header>
     <section id="what">
       <div class="row">
-        <h2 class="large-12 column">What?</h2>
         <div class="large-8 column">
-          <p>Some while ago Mike left the comfort of Florida for colder pastures. But now he is back</p>
-          <p>Join Mike as he shares tech talks, baked goods, and all the homebrew a man can throw together in a month.</p>
+          <h2>What?</h2>
+          <p>Mike made some more beer. Come and try it</p>
         </div>
-        <img class="large-4 column" src="/img/plug-group.jpg">
+        <img class="large-4 column" src="/img/185.JPG">
       </div>
     </section>
     <section id="beer">
@@ -51,12 +50,12 @@
             <p>Bright, summery, and strong. The crowd-pleaser is back</p>
           </div>
           <div class="hishe">
-            <h3>How It Should Have Ended</h3>
-            <p>One year ago I started this brewing experiment. Now I revisit my first disaster-of-a-beer in true Floridian Oktoberfest style</p>
+            <h3>Wheater is Coming</h3>
+            <p>My first attempt at a wheat beer. I haven't tied it yet</p>
           </div>
           <div class="psa">
-            <h3>The Angst</h3>
-            <p>My Pumpkin Spice Ale. Ugh, its so mainstream.</p>
+            <h3>Berry Beer</h3>
+            <p>They wanted a sweet, fruity drink. I relented.</p>
           </div>
           <div class="psa">
             <h3>Do No Wrong</h3>
@@ -67,8 +66,8 @@
     </section>
     <footer>
       <div class="row">
-		<p>Copyright &copy; 2014, the one and only Mike Tracy<br>
-          Coded on the phone.</p>
+		<p>Copyright &copy; 2015, the one and only Mike Tracy<br>
+          Make with ♥ on Linux.</p>
       </div>
     </footer>
    <script src="js/foundation.min.js"></script>
